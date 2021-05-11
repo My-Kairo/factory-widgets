19cfd8cb52d75327b87f7ba49bbf246c65a165f7
Thatohatsi <marciatsoho@gmail.com>
2021-05-11 12:23:08 +0200

    last changes

--- link/radio-bill.js
+++ link/radio-bill.js
@@ -1,9 +1,5 @@
-// get a reference to the sms or call radio buttons
+
 var checkedRadioBtn = document.querySelector(".billItemTypeRadio");
-//get a reference to the add 
-
-
-
 var addButton = document.querySelector(".radioBillAddBtn");
 var calltotal = document.querySelector(".callTotalTwo");
 var smstotal = document.querySelector(".smsTotalTwo");
@@ -18,44 +14,15 @@ var radInsta = radioBttnBill()
 function textBillTotal(){
     
     var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
-    // if (checkedRadioBtn){
-    // var billItemType = checkedRadioBtn.value
-    // // billItemType will be 'call' or 'sms'
-    // }
-
-    // //update the correct total
-    // if (billItemType === "call"){
-    //     totalCalls += 2.75;
-    // }
-    // else if (billItemType === "sms"){
-    //     totalsms += 0.75;
-    // }
+   
     var typeOfBill = checkedRadioBtn.value.trim()
 
     radInsta.billtype(typeOfBill)
 
     calltotal.innerHTML = radInsta.getThecallTotal().toFixed(2);
     smstotal.innerHTML = radInsta.getThesmsTotal().toFixed(2);
-    // var totalBill = totalCalls + totalsms;
     totall.innerHTML = radInsta.billsTotal().toFixed(2)
 
     color.classList.add(radInsta.criticAndwarning())
-
-// if (totalBill >= 30){
-//     color.classList.add("warning");
-// }
-// else if(totalBill <= 30){
-//     color.classList.remove("warning")
-// }
-// if (totalBill >= 50){
-//     color.classList.add("danger");
-// }
-// else if(totalBill <= 50){
-//     color.classList.remove("danger")
-// }
 }
 addButton.addEventListener('click', textBillTotal);
-//in the event listener get the value from the billItemTypeRadio radio buttons
-// * add the appropriate value to the running total
-// * add nothing for invalid values that is not 'call' or 'sms'.
-// * display the latest total on the screen
